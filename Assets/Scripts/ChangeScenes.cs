60075db5701d81fc339f3db6b6a140135aeb673b
crying234 <a42298133@gmail.com>
2021-03-07 12:09:13 +0800

    add button and scenes

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void startgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Tips()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    private Transform PlayerPosition;
+    void Start()
+    {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PlayerPosition.position, speed * Time.deltaTime);
+    }
+}
